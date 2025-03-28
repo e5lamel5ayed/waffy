e44f25a58e258476bd650a8332268ca4c78ef990
ronymostafa2005 <mrere0013@gmail.com>
2025-03-27 21:13:06 -0700

    whatsapp button done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import Nav from "./Components/Nav";
-
-import SectionTwo from "./Sections/Sectiontwo/SectionTwo";
-import Sectionthree from "./Sections/Sectionthree/Sectionthree";
-import Sectionfour from './Sections/Sectionfour/Sectionfour';
-
 import All from './Components/Business/All/all';
 import Home from './Sections/Home';
-
+import WhatsAppButton from "./Components/WhatsAppButton/WhatsAppButton";
 //---------------------------------------------
 
 function App() {
   return (
     <Router>
+        <WhatsAppButton />
       <Routes>
         <Route path="/" element={<Home/> } />
         <Route path="/business" element={<All/> } />
