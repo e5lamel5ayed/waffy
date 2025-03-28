2f07706cdcf1edb999ab2ed8da2edf486b7ee2c3
e5lam el5ayed <eslam25elsayed12@gmail.com>
2025-03-29 00:47:41 +0200

    Done

--- src/Sections/Sectionfour/Sectionfour.jsx
+++ src/Sections/Sectionfour/Sectionfour.jsx
@@ -19,14 +19,14 @@ const Sectionfour = () => {
     { id: 3, image: client4, title: "تشتغل بالعمل الحر ؟", text: "اضمن عربونك وكل مستحقاتك مع وفّي" },
     { id: 4, image: client5, title: "!سلمت  المشروع؟", text: "اضمن مستحقاتك مع وفّي" },
     { id: 5, image: client6, title: "تشتري جهاز مستعمل ؟", text: "اضمن حقوقك وجودة المنتج مع وفّي" },
-    { id: 5, image: client8, title:    "تبيع حسابات رقمية ؟", text: "عزز ثقة عملائك وزيد مبيعاتك مع وفّي" },
+    { id: 5, image: client8, title: "تبيع حسابات رقمية ؟", text: "عزز ثقة عملائك وزيد مبيعاتك مع وفّي" },
     { id: 5, image: client9, title: "تحب تشتري وتبيع العاب ؟", text: "اضمن حقوقك وزيد مبيعاتك مع وفّي " },
   ];
 
   return (
-    <div className="slider-container">
-      <div className="slider-header" style={{ textAlign: "center" ,margin:"8 0px" }}>
-        <h2 style={{ fontSize: "50px", fontWeight: "bold" }}>ندعم العديد من المنتجات والخدمات، كل ما تحتاج إليه لمعاملة آمنة وسهلة</h2>
+    <div className="slider-container bg-white">
+      <div className="slider-header" style={{ textAlign: "center", margin: "8 0px" }}>
+        <h2 style={{ fontWeight: "bold" }}>ندعم العديد من المنتجات والخدمات، كل ما تحتاج إليه لمعاملة آمنة وسهلة</h2>
         <p style={{ fontSize: "25px", maxWidth: "600px", margin: "0 auto" }}>
           صممنا حلولًا تناسب احتياجاتك، بيع على وفي خدماتك مثل (الحسابات الرقمية)، ومنتجاتك مثل: (الإلكترونيات، لوحات السيارات، والألعاب)، والمزيد.
         </p>
@@ -37,14 +37,15 @@ const Sectionfour = () => {
         loop={true}
         autoplay={{ delay: 3000 }}
         modules={[Autoplay]}
+
       >
         {cardsData.map((card) => (
-          <SwiperSlide key={card.id}>
-            <div style={{ textAlign: "center", marginBottom: "10px"  ,padding:"75px"}}>
-              <h3 style={{ fontSize: "35px", fontWeight: "bold" }}>{card.title}</h3>
+          <SwiperSlide  className="d-flex flex-column mt-5 justify-content-center align-items-center" style={{ backgroundColor: '#f8fafb', width: 'calc((100% + var(--space-m))/3 - var(--space-m))', height:'auto' }} key={card.id}>
+            <div className="pt-5" style={{ textAlign: "end", marginBottom: "10px", backgroundColor: '#f8fafb' }}>
+              <h3 style={{ fontSize: "clamp(1.8rem,calc(0.67vw + 1.59rem),2.5rem)", fontWeight: "bold" }}>{card.title}</h3>
               <p style={{ fontSize: "20px", marginTop: "5px" }}>{card.text}</p>
             </div>
-            <img src={card.image} alt={`Card ${card.id}`} className="slider-img" />
+            <img src={card.image} alt={`Card ${card.id}`} className="slider-img text-center w-50" />
           </SwiperSlide>
         ))}
       </Swiper>
