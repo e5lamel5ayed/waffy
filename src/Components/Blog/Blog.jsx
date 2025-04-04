965ced181d6ec7d474372a01ec92758ca04d23eb
AmeraBakr <meraabadran3@gmail.com>
2025-04-04 16:01:57 +0200

    cards

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import Nav from "../Nav";
 import Herro from "./Herro/Herro";
 import Cards from "./Cards/Cards";
+// import Footer from "../../Sections/Footer"
 
 function Blog() {
   return (
 
     <div className="Blog">
-      <Nav />
-      <Herro />
-      <Cards />
+      <Nav/>
+      <Herro/>
+      <Cards/>
+      {/* <Footer/> */}
 
     </div>
   );
